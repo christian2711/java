d2d2dd6bc1afc61bd2d60e6c90c182490d99732a
christian2711 <distribuidoracalamuchita@gmail.com>
2023-04-10 09:08:52 -0300

    uno

---
+++ primerClase/src/main/java/com/mycompany/primerclase/PrimerClase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerclase;
+
+/**
+ *
+ * @author tante
+ */
+public class PrimerClase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
